afd931621119024aaa44d8cc481b4fa1ff44a4b9
sunchenwei <sun.chenwei@puscene.com>
2020-07-12 21:52:11 +0800

    链表

---
+++ leetcode/question141.go
@@ -0,0 +1,37 @@
+/**
+https://leetcode-cn.com/problems/linked-list-cycle/
+
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+*/
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
+// 双指针
